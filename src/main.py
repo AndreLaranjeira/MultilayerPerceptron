90151ba3932e6f3509316344e0a8825848599cc4
André Laranjeira <andrecaldaslaranjeira@gmail.com>
2019-04-24 17:31:18 -0300

    Implemented data extraction *Uploaded data files for the MNIST dataset. *Added code to use the 'python-mnist' package to extract the data from the MNIST dataset.

--- src/main.py
+++ src/main.py
@@ -8,3 +8,14 @@ import theano
 # User imports:
 
 # Main function:
+
+# Configuration for 'python-mnist' package:
+mndata = MNIST(path='./data', return_type='numpy')
+mndata.test_img_fname = 't10k-images.idx3-ubyte'
+mndata.test_lbl_fname = 't10k-labels.idx1-ubyte'
+mndata.train_img_fname = 'train-images.idx3-ubyte'
+mndata.train_lbl_fname = 'train-labels.idx1-ubyte'
+
+# Data extraction:
+train_images, train_labels = mndata.load_training()
+test_images, test_labels = mndata.load_testing()
