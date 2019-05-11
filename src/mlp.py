c1a5d7fa8712461e9d23a675a1e7224193b4ff98
Luiz <luiz@aluno.unb.br>
2019-05-10 21:05:04 -0300

    HiddenLayer implemented Some variable names have been changed in order to keep easy compatibility with tutorial MLP supports many hidden layers

--- src/mlp.py
+++ src/mlp.py
@@ -3,6 +3,7 @@
 # Package imports:
 import numpy as np
 import theano
+import theano.tensor as T
 
 # Classes:
 
@@ -17,28 +18,44 @@ import theano
 
 class MultilayerPerceptron:
 
-    def __init__(self, input, n_inputs, n_outputs, n_hlayers, hlayer_sizes):
+    def __init__(self, layer_input, n_inputs, n_outputs, n_hlayers, hlayer_sizes):
 
         # Initialize the input:
-        self.input = input
+        self.input = layer_input
 
         # WARNING: This implementation is only temporary! Changes should be made
         # to it, to account for the HiddenLayer implementation!!!
-
+        
+        self.hidden_layers = []
+        
+        # Create n_hlayers of hidden layers
+        for i in range(n_hlayers):
+            # Append hiddenLayer
+            self.hidden_layers.append(
+                HiddenLayer(
+                    rng=np.random.RandomState(1234), # Could be passed by argument
+                    layer_input=self.hidden_layers[i-1].output if (i != 0) else layer_input,
+                    n_in=hlayer_sizes[i-1] if (i != 0) else n_inputs,
+                    n_out=hlayer_sizes[i],
+                    activation=T.tanh
+                )
+            )
+        
+        
         # Initializing the output layer:
-        self.output_layer = OutputLayer(input = input,
-                                        n_inputs = n_inputs,
+        self.output_layer = OutputLayer(layer_input = self.hidden_layers[-1].output, # Last hidden layer output
+                                        n_inputs = hlayer_sizes[-1], # Last hidden layer size
                                         n_outputs = n_outputs)
 
         # Record the parameters:
-        self.params = self.output_layer.params
+        self.params = self.output_layer.params + list(sum(map(lambda x: x.params, self.hidden_layers),[]))
 
         # Cost metrics:
         ## L1 cost metric:
-        self.L1 = abs(self.output_layer.weights).sum()
+        self.L1 = abs(self.output_layer.W).sum() + sum(map(lambda x: x.W.sum(), self.hidden_layers))
 
         ## L2 squared cost metric:
-        self.L2_sqr = (self.output_layer.weights ** 2).sum()
+        self.L2_sqr = (self.output_layer.W ** 2).sum() + sum(map(lambda x: (x.W ** 2).sum(), self.hidden_layers))
 
         # Classification metrics:
         ## MLP errors:
@@ -59,23 +76,23 @@ class OutputLayer:
 
         # Initialize layer_input, weights and biases:
         self.layer_input = layer_input
-        self.weights = theano.shared(value = np.zeros(
+        self.W = theano.shared(value = np.zeros(
                                              (n_inputs, n_outputs),
                                              dtype=theano.config.floatX),
-                                     name = 'weights',
+                                     name = 'W',
                                      borrow = True)
-        self.biases = theano.shared(value = np.zeros(
+        self.b = theano.shared(value = np.zeros(
                                             (n_outputs,),
                                             dtype=theano.config.floatX),
-                                    name = 'bias',
+                                    name = 'b',
                                     borrow = True)
 
         # Record the parameters:
-        self.params = [self.weights, self.biases]
+        self.params = [self.W, self.b]
 
         # Probability function:
-        self.y_probabilities = T.nnet.softmax(T.dot(layer_input, self.weights)
-                                              + self.biases)
+        self.p_y_given_x = T.nnet.softmax(T.dot(layer_input, self.W)
+                                              + self.b)
 
         # Prediction function:
         self.predict = T.argmax(self.p_y_given_x, axis=1)
@@ -102,3 +119,46 @@ class OutputLayer:
     # Negative logarithm likehood function:
     def negative_log_likehood(self, y):
         return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
+        
+        
+class HiddenLayer:
+    
+    def __init__(self, rng, layer_input, n_in, n_out, W=None, b=None, activation=T.tanh):
+        
+        # Set input object variable
+        self.input = layer_input
+        
+        # Set weights using random number generator (rng) given
+        if W is None:
+            # Suggestion of Xavier
+            W_values = np.asarray(
+                rng.uniform(
+                    low=-np.sqrt(6. / (n_in + n_out)),
+                    high=np.sqrt(6. / (n_in + n_out)),
+                    size=(n_in, n_out)
+                ),
+                dtype=theano.config.floatX
+            )
+            if activation == theano.tensor.nnet.sigmoid:
+                W_values *= 4
+                
+            # Set as theano variable
+            W = theano.shared(value=W_values, name='W', borrow=True)
+            
+        # Set all initial biases to zero
+        if b is None:
+            b_values = np.zeros((n_out,), dtype=theano.config.floatX)
+            b = theano.shared(value=b_values, name='b', borrow=True)
+        
+        self.W = W
+        self.b = b
+        
+        lin_output = T.dot(layer_input, self.W) + self.b
+        
+        # Sets activation function over output if there's such function
+        self.output = (
+            lin_output if activation is None
+            else activation(lin_output)
+        )
+        
+        self.params = [self.W, self.b]
